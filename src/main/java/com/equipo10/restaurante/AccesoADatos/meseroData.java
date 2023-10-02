7923098633d692611f50cbeac3668322ac5b77d2
ComettoLucas <comettolucas@gmail.com>
2023-10-02 17:41:28 -0300

    Update meseroData.java

--- src/main/java/com/equipo10/restaurante/AccesoADatos/meseroData.java
+++ src/main/java/com/equipo10/restaurante/AccesoADatos/meseroData.java
@@ -5,6 +5,13 @@
 
 package com.equipo10.restaurante.AccesoADatos;
 
+import com.equipo10.restaurante.Entidades.Mesero;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Lucas Cometto
@@ -15,5 +22,32 @@ public class meseroData {
     private List<Pedido> pedido;
     private Mesa mesa;
     */
+    //●	Los meseros pueden listar pedidos cobrados en el día. Anular pedidos. 
+    //●	Listar los pedidos que cobro un mesero en particular en el día.
+    //●	Cada pedido es cobrado por un mesero y cada mesero cobra muchos pedidos.
+      private Connection con = null;
+      public meseroData() {
+        con = Conexion1.getConexion();
+        }
+        public void crearMozo(Mesero mesero) {
+
+        String sql = "INSERT INTO mesero (nombre) VALUES (?)";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, mesero.getNombreApellido());
+            ps.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Mesero Agregado");
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Mesero" + ex.getMessage());
+
+        }
+
+    }
+
+    
+    
+    
     
 }
