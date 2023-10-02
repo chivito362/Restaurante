8d9f6c5a2a9d0d20475c3230a9440a020a3c952f
ComettoLucas <comettolucas@gmail.com>
2023-10-02 17:24:53 -0300

    meseroData y conexion1

---
+++ src/main/java/com/equipo10/restaurante/AccesoADatos/meseroData.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.equipo10.restaurante.AccesoADatos;
+
+/**
+ *
+ * @author Lucas Cometto
+ */
+public class meseroData {
+    /* private int idMesero;
+    private String nombreApellido;
+    private List<Pedido> pedido;
+    private Mesa mesa;
+    */
+    
+}
