568afb5f32de8e5eb3127a82d93e35223ffb629b
Alexaurio1212 <141673099+Alexaurio1212@users.noreply.github.com>
2023-10-04 16:16:21 -0300

    data

--- src/main/java/com/equipo10/restaurante/AccesoADatos/PedidoData.java
+++ src/main/java/com/equipo10/restaurante/AccesoADatos/PedidoData.java
@@ -47,8 +47,47 @@ public class PedidoData {
         
     }
   
-  public void editarPedido(){
+  public void editarPedido(Pedido pedido){
+  String sql = "UPDATE alumno SET mesa = ? , mesero = ?, detalle = ?, totalPedido = ?, entregado= ?, pagado= ? WHERE idPedido = ?";
+        PreparedStatement ps = null;
+
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, pedido.getMesa().getIdMesa());
+            ps.setInt(2, pedido.getMesero().getIdMesero());
+            ps.setInt(3, pedido.getDetalle().getIdDetalle());
+            ps.setDouble(4, pedido.getTotalPedido());
+            ps.setBoolean(5, pedido.isEntregado());
+            ps.setBoolean(6, pedido.isPagado());
+            int exito = ps.executeUpdate();
+
+            if (exito == 1) {
+                JOptionPane.showMessageDialog(null, "Modificado Exitosamente.");
+            } else {
+                JOptionPane.showMessageDialog(null, "No se encontro pedido");
+            }
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder al pedido ");
+        }
+
+    }
+  
+  public void eliminarPedido(int id){
+  try {
+            String sql = "UPDATE pedido SET entregado = 0, pagado = 0 WHERE idAlumno = ? ";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+            int fila = ps.executeUpdate();
+
+            if (fila == 1) {
+                JOptionPane.showMessageDialog(null, " Se eliminó pedido.");
+            }
+            ps.close();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, " Error al acceder al pedido");
+        }
+    }
   
   }
     
-}
