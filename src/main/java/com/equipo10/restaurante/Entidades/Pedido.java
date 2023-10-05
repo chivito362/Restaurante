e9923387aa8a6bd2f20bbc123464a175f5dc8d2e
Alexaurio1212 <141673099+Alexaurio1212@users.noreply.github.com>
2023-10-05 17:08:53 -0300

    pedido

--- src/main/java/com/equipo10/restaurante/Entidades/Pedido.java
+++ src/main/java/com/equipo10/restaurante/Entidades/Pedido.java
@@ -3,13 +3,75 @@ package com.equipo10.restaurante.Entidades;
 
 import java.util.List;
 
-
 public class Pedido {
+
     private int idPedido;
     private Mesa mesa;
     private Mesero mesero;
-    private List<Producto> productos;
-    private double totalPedido;
     private boolean entregado;
     private boolean pagado;
+
+    public Pedido(Mesa mesa, Mesero mesero, boolean entregado, boolean pagado) {
+        this.mesa = mesa;
+        this.mesero = mesero;
+        this.entregado = entregado;
+        this.pagado = pagado;
+    }
+
+    public Pedido(int idPedido, Mesa mesa, Mesero mesero, boolean entregado, boolean pagado) {
+        this.idPedido = idPedido;
+        this.mesa = mesa;
+        this.mesero = mesero;
+        this.entregado = entregado;
+        this.pagado = pagado;
+    }
+
+    public Pedido() {
+    }
+
+    public int getIdPedido() {
+        return idPedido;
+    }
+
+    public void setIdPedido(int idPedido) {
+        this.idPedido = idPedido;
+    }
+
+    public Mesa getMesa() {
+        return mesa;
+    }
+
+    public void setMesa(Mesa mesa) {
+        this.mesa = mesa;
+    }
+
+    public Mesero getMesero() {
+        return mesero;
+    }
+
+    public void setMesero(Mesero mesero) {
+        this.mesero = mesero;
+    }
+
+    public boolean isEntregado() {
+        return entregado;
+    }
+
+    public void setEntregado(boolean entregado) {
+        this.entregado = entregado;
+    }
+
+    public boolean isPagado() {
+        return pagado;
+    }
+
+    public void setPagado(boolean pagado) {
+        this.pagado = pagado;
+    }
+
+    @Override
+    public String toString() {
+        return "Pedido{" + "idPedido=" + idPedido + ", mesa=" + mesa + ", mesero=" + mesero + ", productos=" + ", totalPedido="+ ", entregado=" + entregado + ", pagado=" + pagado + '}';
+    }
+
 }
