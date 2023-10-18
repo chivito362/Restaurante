7a871279bb3a05555de068c0c8026a75046e2153
chivito362 <Chivito362@gmail.com>
2023-10-18 15:32:43 -0300

    CalcularTotalPedido

--- src/main/java/com/equipo10/restaurante/AccesoADatos/PedidoData.java
+++ src/main/java/com/equipo10/restaurante/AccesoADatos/PedidoData.java
@@ -300,14 +300,13 @@ public class PedidoData {
     }
 public double calcularTotalPedido(int idPedido){
     int total=0;
-    String sql="";
+    String sql="SELECT producto.Precio, pedidodetalle.cantidad FROM pedidodetalle JOIN producto ON pedidodetalle.idProducto = producto.idProducto WHERE pedidodetalle.idPedido = ?";
         try {
             PreparedStatement ps=con.prepareStatement(sql);
             ps.setInt(1, idPedido);
             ResultSet rs=ps.executeQuery();
-            if(rs.next()){
-                total=rs.getInt("total");
-                return total;
+            while(rs.next()){
+                total=rs.getInt("Precio") * rs.getInt("cantidad");
             }
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Error consulta sql al traer total");
